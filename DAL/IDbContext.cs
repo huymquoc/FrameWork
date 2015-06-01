b30741ae1c68c44fa4f987985d4af3772649243f
huymquoc <huymquoc@gmail.com>
2015-06-01 07:41:52 +0700

    First commit

---
+++ DAL/IDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DAL
+{
+    public interface IDbContext
+    {
+      //  IDbSet<T> Set<T>() where T : class ;
+    }
+}
